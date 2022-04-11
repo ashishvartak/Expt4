7e7257a253153ed12d1e0372f007de1afc10be76
ashishvartak <vartak.a@gmail.com>
2022-04-11 19:29:04 +0530

    commit

--- expt4/lib/main.dart
+++ expt4/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-//Example for explaining Layout using Container
+//Example for explaining Layout using Gridview
 void main() {
   runApp(MyApp());
 }
@@ -25,19 +25,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(10));
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count,
+      (i) => Container(
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+          ));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("IT Department"),
         ),
-        body: SafeArea(
-          // Container implementation
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.green,
-          ),
-        ));
+        body: Center(child: _buildGrid()));
   }
 }
